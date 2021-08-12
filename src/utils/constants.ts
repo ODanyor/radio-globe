bab7db446a264b3e178ad1ccf72852c02a432a0b
ODanyor <doniyorgit@gmail.com>
2021-08-12 20:31:09 +0500

    cache variable name constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+// HEADDING: cache varaibles
+export const IMMORTAL_CHANNEL = '_immortal|channel';
+export const IMMORTAL_CHANNEL_LOCKED = '_immortal|channel-locked';
+export const IMMORTAL_VOLUME = '_immortal|volume';
+export const IMMORTAL_LOCATION = '_immortal|location';
