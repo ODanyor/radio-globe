3febee6b2b4561dc21361c9e8e90f49e50afb101
ODanyor <doniyorgit@gmail.com>
2021-08-16 09:35:36 +0500

    modified useAudioPlayer

--- src/hooks/useAudioPlayer.ts
+++ src/hooks/useAudioPlayer.ts
@@ -4,16 +4,13 @@ import { AudioPlayer } from 'types';
 export function useAudioPlayer({src, playing, muted, volume}: AudioPlayer) {
   const audio = useMemo(() => new Audio(), []);
 
-  useEffect(() => {
-    audio.setAttribute('src', src);
-    playing && audio.play().catch(() => null);
-  }, [audio, src]); // eslint-disable-line
+  useEffect(() => { audio.setAttribute('src', src) }, [audio, src]);
   useEffect(() => {
     if (playing) {
       audio.load();
       audio.play();
-    } else audio.pause() ;
-  }, [audio, playing]);
+    } else audio.pause();
+  }, [src, audio, playing]);
   useEffect(() => { audio.muted = muted }, [audio, muted]);
   useEffect(() => { audio.volume = volume }, [audio, volume]);
 
