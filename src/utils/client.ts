eb066c4991c5ec2b1de48ed620017114f0b9a501
ODanyor <doniyorgit@gmail.com>
2021-08-17 13:02:59 +0500

    client default configs

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,8 +2,15 @@ const stand = process.env.NODE_ENV === 'production'
   ? process.env.BASE_URL
   : 'http://localhost:8000/api';
 
+const defaults = {
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+};
+
 function client(url: string, options = {}) {
-  return fetch(`${stand}/${url}`, options);
+  return fetch(`${stand}/${url}`, {...options, ...defaults});
 }
 
 export default client;
