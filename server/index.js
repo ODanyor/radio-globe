fb45a056947b8b6ce967f76a5084685e78833663
ODanyor <doniyorgit@gmail.com>
2021-08-17 12:55:53 +0500

    get favorites

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors')
-
+const cors = require('cors');
+const router = express.Router();
 const app = express();
+
 const port = 8000;
 const address = 'http://radio.garden/api/ara/content';
 
 app.use(cors());
-
 
 // Radio Garden API
 const getPlacesUrl = () => `${address}/places`;
@@ -15,43 +15,48 @@ const getChannelsUrl = (stationId) => `${address}/page/${stationId}`;
 const getAllChannelsUrl = (stationId) => `${address}/page/${stationId}/channels`;
 const getChannelUrl = (channelId) => `${address}/channel/${channelId}`;
 const getStreamUrl = (channelId) => `${address}/listen/${channelId}/channel.mp3`;
+const getFavorites = () => `${address}/favorites`;
 
-app.get('/api/places', async function(req, res) {
+router.get('/api/places', async function(req, res) {
   await axios.get(getPlacesUrl()).then(
     ({data}) => res.send(data),
     (error) => res.status(500).send(error)
   );
 });
-app.get('/api/page/:stationId', async function (req, res) {
+router.get('/api/page/:stationId', async function(req, res) {
   const { stationId } = req.params;
   await axios.get(getChannelsUrl(stationId)).then(
     ({data}) => res.send(data),
     (error) => res.status(500).send(error)
   );
 });
-app.get('/api/channel/:channelId', async function (req, res) {
+router.get('/api/channel/:channelId', async function(req, res) {
   const { channelId } = req.params;
   await axios.get(getChannelUrl(channelId)).then(
     ({data}) => res.send(data),
     (error) => res.status(500).send(error)
   );
 });
-app.get('/api/channel/:stationId/channels', async function (req, res) {
+router.get('/api/channel/:stationId/channels', async function(req, res) {
   const { stationId } = req.params;
   await axios.get(getAllChannelsUrl(stationId)).then(
     ({data}) => res.send(data),
     (error) => res.status(500).send(error)
   );
 });
-app.get('/api/listen/:channelId', async function (req, res) {
+router.get('/api/listen/:channelId', async function(req, res) {
   const { channelId } = req.params;
   return res.send({data: getStreamUrl(channelId)});
-  // await axios.get(getStreamUrl(channelId)).then(
-  //   ({data}) => res.send(data),
-  //   (error) => res.status(500).send(error)
-  // );
+});
+router.post('/api/favorites', async function(req, res) {
+  const favorites = req.body.favorites;
+  await axios.post(getFavorites(), {favorites}).then(
+    ({data}) => res.send(data),
+    (error) => res.status(500).send(error)
+  );
 });
 
+app.use('/', router);
 app.listen(port, function () {
   console.log(`Example app listening at http://localhost:${port}`);
 });
