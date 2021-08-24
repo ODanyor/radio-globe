631139df7c9f1d0a4133a9d5f4746c103dcd1ea2
ODanyor <doniyorgit@gmail.com>
2021-08-24 17:02:44 +0500

    useWindowSize hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+interface WindowSize {
+  width: number;
+  height: number;
+};
+
+export function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handler() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    handler();
+
+    window.addEventListener('resize', handler);
+
+    return () => window.removeEventListener('resize', handler);
+  }, []);
+
+  return windowSize;
+}
