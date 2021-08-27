f2e563342462684dc5c0f1045b3a9891ab2a91d1
ODanyor <doniyorgit@gmail.com>
2021-08-27 15:23:20 +0500

    current playing station in Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,11 @@
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Button, IconButton } from '@chakra-ui/react';
 import { FiChevronLeft } from 'react-icons/fi';
+import { useBrowserContext } from 'services/browser';
+import { usePlayerContext, setPlaying } from 'services/player';
+import { getItemId } from 'utils/data';
 import { ContentItem, Params } from 'types';
+import { FiSquare, FiPlay } from 'react-icons/fi';
 
 type ContentProps = {
   content: ContentItem;
@@ -10,6 +14,8 @@ type ContentProps = {
 function Content({ content }: ContentProps) {
   const history = useHistory();
   const {option} = useParams<Params>();
+  const [{channelId}] = useBrowserContext();
+  const [{playing, loading, url}, playerDispatch] = usePlayerContext();
 
   function getContent() {
     return content.items.map((item: any, index: number) => {
@@ -19,6 +25,31 @@ function Content({ content }: ContentProps) {
         subtitle = item.page.subtitle;
       }
       if (item.url) path = item.url;
+
+      let itemId = item.id || getItemId(item);
+      if (channelId === itemId && url) return (
+        <Flex
+          key={index}
+          m="0 -1rem"
+          p=".3rem 1rem"
+          minH="3.5rem"
+          justifyContent="space-between"
+          _hover={{ bg: "#000011" }}>
+          <Box>
+            <Heading as="h4" size="md">{item.title}</Heading>
+            <Text>{subtitle}</Text>
+          </Box>
+          <IconButton
+            aria-label="play-toggle"
+            icon={playing ? <FiSquare /> : <FiPlay />}
+            onClick={() => setPlaying(playerDispatch, !playing)}
+            disabled={loading}
+            isLoading={loading}
+            background="none"
+            size="lg"
+            _hover={{}} />
+        </Flex>
+      );
       
       return (
         <Link key={index} to={path}>
