d6d56bfddce8eb2b03a71636e5cc9ddacd387c09
ODanyor <doniyorgit@gmail.com>
2021-08-31 09:50:34 +0500

    initial AudioVisualizer component

--- src/components/AudioVisualizer.tsx
+++ src/components/AudioVisualizer.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import { Box } from '@chakra-ui/react';
+import Wave from 'wave-visualizer';
 
 function AudioVisualizer() {
-  return <Box position="absolute" width="100%" height="100%" bgColor="#000011" />;
+  const [wave] = useState(new Wave());
+
+  // TODO: width of the wave should be 100% by height and width
+  // wave.fromStream(stream, 'output', {
+  //   colors: ['red', 'white', 'blue'],
+  // });
+
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+    })
+    .then(function (stream) {
+      wave.fromStream(stream, 'output', {
+        colors: ['red', 'white', 'blue'],
+      });
+    })
+    .catch(function (err) {
+      console.log(err.message);
+    });
+
+  return (
+    <Box
+      pos="absolute"
+      w="100%"
+      h="100%"
+      bgColor="#000011">
+      <canvas id="output" height="100vh" width="100vw"></canvas>
+    </Box>
+  );
 }
 
 export default AudioVisualizer;
