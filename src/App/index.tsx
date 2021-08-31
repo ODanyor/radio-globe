1422c7048a24522aca66010b75fb1291da4e6b47
ODanyor <doniyorgit@gmail.com>
2021-08-31 09:51:22 +0500

    increased timeout of AppLoading

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,7 +5,7 @@ import { AppLoading, Globe, AudioVisualizer } from 'components';
 function App() {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {setTimeout(() => setLoading(false), 2000)}, []);
+  useEffect(() => {setTimeout(() => setLoading(false), 3000)}, []);
 
   if (loading) return <AppLoading />;
 
